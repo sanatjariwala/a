70b63628ba59e2abb26182fabe3a2de976f793df
sanatjariwala <sanatjariwala549@gmail.com>
2025-09-29 15:23:24 +0530

    some changes

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,7 +6,7 @@ function Products() {
       <ul>
         <h1>Products list 2</h1>
         <h2>
-          <Link to="/Cars/Audi">Cars</Link>
+          <Link to="/Cars/Audi">Cars for new branch</Link>
         </h2>
         <h2>
           <Link to="/Bikes/Yamaha">Bikes</Link>
